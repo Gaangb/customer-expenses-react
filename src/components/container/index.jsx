2292b0c2c2f5e8046e523011201ae1ddfb29e6b7
Gaangb <angelob4stos@gmail.com>
2023-09-15 12:36:47 -0300

    feat: criando container

---
+++ src/components/container/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style.css"
+
+export default function Container({customClass, children}){
+    return(
+        <div className={customClass}>
+            {children}
+        </div>
+    )
+}
