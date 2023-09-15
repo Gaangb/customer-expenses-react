a8b0eb4664bb9dd49347296d61503ef0ca65b02e
Gaangb <angelob4stos@gmail.com>
2023-09-15 12:40:12 -0300

    feat: criando search

---
+++ src/components/inputSearch/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+import {BiSearch} from "react-icons/bi"
+
+export default function InputSearch({onChange}) {
+  return (
+    <div className='container_search'>
+        
+        <BiSearch/>
+        <input type="text" placeholder="Pesquisar" onChange={onChange} className="search"/> 
+
+    </div>
+     );
+}
