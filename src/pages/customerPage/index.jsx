600a2700b22420ec2a04bbc3f82453684af6ef2e
Gaangb <angelob4stos@gmail.com>
2023-09-16 00:00:24 -0300

    feat: Atualizando o customerpage

--- src/pages/customerPage/index.jsx
+++ src/pages/customerPage/index.jsx
@@ -1,18 +1,14 @@
-import React from 'react'
-import './style.css'
-import Container from '../../components/container';
-import InputSearch from '../../components/inputSearch';
-import ExpenseList from '../../components/expensesList';
-
+import React from "react";
+import "./style.css";
+import Container from "../../components/container";
+import ExpenseList from "../../components/expensesList";
+import Header from "../../components/header";
 
 export default function CustomerPage() {
-
-
   return (
-    <Container customClass='container'>
-      <InputSearch/>
+    <Container customClass="customer_container container">
+      <Header />
       <ExpenseList />
     </Container>
   );
 }
-
