9f7fba282987ea90ce0f6e270bbe352835e82e45
Gaangb <angelob4stos@gmail.com>
2023-09-16 16:43:16 -0300

    feat: incrementando um title ao input

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './style.css'
 
-export default function Input({type, placeholder, onChange, maxLength, customClass, value, name}) {
+export default function Input({title, type, placeholder, onChange, maxLength, customClass, value, name}) {
   return (
-    <input type={type} placeholder={placeholder} onChange={onChange} maxLength={maxLength} className={customClass} value={value} name={name}/>
+    <input title={title} type={type} placeholder={placeholder} onChange={onChange} maxLength={maxLength} className={customClass} value={value} name={name}/>
   );
 }
 
