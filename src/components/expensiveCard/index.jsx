fc4da668e128454a20c264bb17a6f4d9f6e3bf46
Gaangb <angelob4stos@gmail.com>
2023-09-15 23:57:38 -0300

    feat: Atualizando o expensiveCard

--- src/components/expensiveCard/index.jsx
+++ src/components/expensiveCard/index.jsx
@@ -1,11 +1,57 @@
-import './style.css'
-export default function ExpensiveCard({id, taskType, taskValue, taskDate, taskDescription}){
-    return(
+import { VscEdit, VscTrash } from "react-icons/vsc";
+import "./style.css";
+import { useCustomer } from "../../hooks/CustomerHooks";
+
+export default function ExpensiveCard({
+  id,
+  expenseType,
+  expenseValue,
+  expenseDate,
+  expenseDescription,
+}) {
+  const {
+    showFormExpense,
+    currentExpense,
+    expenses,
+    setShowFormExpense,
+    setCurrentExpense,
+    setExpenses,
+  } = useCustomer();
+
+  function handleEdit(id){
+    setShowFormExpense(true)
+    setCurrentExpense(expenses.filter(expense => expense.id === id))
+    // const updateExpenses = expenses.filter(expense => expense.id === id)
+    // setExpenses(updateExpenses)
+    console.log(currentExpense)
+  }
+
+  function handleDelete(id){
+    const updateExpenses = expenses.filter(expense => expense.id !== id)
+    setExpenses(updateExpenses)
+    console.log(id)
+  }
+
+  return (
     <div className="container_card">
-        <p>{taskType}</p>
-        <p>R${taskValue}</p>
-        <p>{taskDate}</p>
-        <p>{taskDescription}</p>
+      <div className="container_card_texts">
+        <p>{expenseType}</p>
+        <p>{expenseDescription}</p>
+      </div>
+      <div className="container_card_rigth">
+        <div className="container_card_texts">
+          <p>R$ {expenseValue}</p>
+          <p>{expenseDate}</p>
+        </div>
+        <div className="container_card_buttons">
+          <button onClick={() => handleEdit(id)}>
+            <VscEdit />
+          </button>
+          <button onClick={() => handleDelete(id)}>
+            <VscTrash />
+          </button>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
