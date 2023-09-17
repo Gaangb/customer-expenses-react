e557fe8d2306e26ce83f7f36712f097d096950b3
Gaangb <angelob4stos@gmail.com>
2023-09-17 01:07:10 -0300

    feat: Criando NavBar

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import { CustomerProvider } from './hooks/CustomerHooks';
 import CustomerPage from './pages/customerPage';
 import { LoginPage } from './pages/loginPage';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 import { useCustomer } from './hooks/CustomerHooks';
+import NavBar from './components/navBar';
 
 function App() {
   return (
     <Router>
-      <div className='container_geral'>
-        <CustomerProvider>
-          <Routes>
-            <Route path="/" element={<LoginPage/>}/>
-            <Route path="/customer/:id" element={<CustomerPage/>}/>
-          </Routes>
-        </CustomerProvider>
-      </div>
+        <NavBar />
+        <div className='container_geral'>
+          <CustomerProvider>
+            <Routes>
+              <Route path="/" element={<LoginPage/>}/>
+              <Route path="/customer/:id" element={<CustomerPage/>}/>
+            </Routes>
+          </CustomerProvider>
+        </div>
     </Router>
   );
 }
---
+++ src/components/navBar/index.jsx
@@ -0,0 +1,9 @@
+import logoZenze from "../../assets/logoZenze.png"
+import "./style.css"
+export default function NavBar(){
+    return  (
+        <div className="buttonLogo">
+            <img src={logoZenze} alt="" />  
+        </div>
+    )     
+}
