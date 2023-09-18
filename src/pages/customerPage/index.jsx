4f2c15ec6207421454a1ef5424583cb41390a775
Gaangb <angelob4stos@gmail.com>
2023-09-17 22:00:37 -0300

    feat: Adicionando params no customer

--- src/pages/customerPage/index.jsx
+++ src/pages/customerPage/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Container from "../../components/container";
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import ExpenseList from "../../components/expensesList";
 import Header from "../../components/header";
 
 export default function CustomerPage() {
+  let { id } = useParams();
+  //console.log("idCustomer", id);
   const location = useLocation();
   const userProfile = location.state?.value;
-  console.log(userProfile)
+  //console.log(userProfile)
   return (
     <Container customClass="customer_container container">
       <Header />
